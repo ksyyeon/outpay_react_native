1f9d500643dca558bbfbe2ab874b85967d83bc62
ksyyeon <suyeonmin@naver.com>
2021-07-22 17:52:09 +0900

    뒤로가기 두번 누르면 종료

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
--- App.js
+++ App.js
@@ -6,17 +6,64 @@
  * @flow strict-local
  */
 
- import React from 'react';
- import {WebView} from 'react-native-webview';
- 
- export default class App extends React.Component {
-   render() {
-     return (
-       <WebView
-         source={{
-           uri: 'http://172.16.21.58/osiris/.development/appIndex.html?mode=devMode&telNum=010-5060-3160#',
-         }}
-       />
-     );
-   }
- }
+import React from 'react';
+import {BackHandler, Platform, ToastAndroid} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+export default class App extends React.Component {
+  webView = {
+    ref: null,
+  };
+
+  onWebViewMessage = e => {};
+
+  onLoadWebView = () => {};
+
+  onBackPress = () => {
+    // if (this.webView.canGoBack && this.webView.ref) {
+    //   this.webView.ref.goBack();
+    //   return true;
+    // }
+    // return false;
+    if (this.exitApp === undefined || !this.exitApp) {
+      ToastAndroid.show('한번 더 누르시면 종료됩니다.', ToastAndroid.SHORT);
+      this.exitApp = true;
+
+      this.timeout = setTimeout(() => {
+        this.exitApp = false;
+      }, 2000);
+    } else {
+      clearTimeout(this.timeout);
+
+      BackHandler.exitApp();
+    }
+    return true;
+  };
+
+  // 이벤트 동작
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  // 이벤트 해제
+  componentWillUnmount() {
+    this.exitApp = false;
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  render() {
+    return (
+      <WebView
+        source={{
+          uri: 'http://172.16.21.58/osiris/.development/appIndex.html?mode=devMode&telNum=010-5060-3160#',
+        }}
+        ref={webView => {
+          this.webView.ref = webView;
+        }}
+        javaScriptEnabled={true}
+        onLoad={this.onLoadWebView}
+        onMessage={this.onWebViewMessage}
+      />
+    );
+  }
+}
