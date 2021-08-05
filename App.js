1249d7d96c1393196fadff16ca87b078e6ff9845
ksyyeon <suyeonmin@naver.com>
2021-08-05 17:07:30 +0900

    openBrowser() 추가

--- App.js
+++ App.js
@@ -7,11 +7,12 @@
  */
 
 import React from 'react';
-import {BackHandler, Platform, ToastAndroid} from 'react-native';
+import {BackHandler, Linking, Platform, ToastAndroid} from 'react-native';
 import {WebView} from 'react-native-webview';
 import createInvoke from 'react-native-webview-invoke/native';
 // import asUtils from './asyncStorageUtils.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SendIntentAndroid from 'react-native-send-intent';
 
 export default class App extends React.Component {
     //   webView = {
@@ -26,6 +27,38 @@ export default class App extends React.Component {
                 'http://172.16.21.58/osiris/.development/appIndex.html?mode=devMode&telNum=010-5060-3160#',
         };
         this.invoke = createInvoke(() => this.webView);
+    }
+
+    // 이벤트 동작
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+        this.invoke.define('exitApp', this.exitApp);
+        this.invoke.define('setUserInfo', this.setUserInfo);
+        this.invoke.define('getUserInfo', this.getUserInfo);
+        this.invoke.define('setUserInfoValue', this.setUserInfoValue);
+        this.invoke.define('getUserInfoValue', this.getUserInfoValue);
+        this.invoke.define('openBrowser', this.openBrowser);
+        this.invoke.define('openShareChooser', this.openBrowser);
+    }
+
+    // 이벤트 해제
+    componentWillUnmount() {
+        // this.exitApp = false;
+        BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+    }
+
+    render() {
+        return (
+            <WebView
+                source={{uri: this.state.webViewUrl}}
+                ref={webView => {
+                    this.webView = webView;
+                }}
+                javaScriptEnabled={true}
+                onLoad={this.onLoadWebView}
+                onMessage={this.invoke.listener}
+            />
+        );
     }
 
     onLoadWebView = () => {};
@@ -88,33 +121,24 @@ export default class App extends React.Component {
         await AsyncStorage.setItem('@OutpayCert', JSON.stringify(json));
     };
 
-    // 이벤트 동작
-    componentDidMount() {
-        BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
-        this.invoke.define('exitApp', this.exitApp);
-        this.invoke.define('setUserInfo', this.setUserInfo);
-        this.invoke.define('getUserInfo', this.getUserInfo);
-        this.invoke.define('setUserInfoValue', this.setUserInfoValue);
-        this.invoke.define('getUserInfoValue', this.getUserInfoValue);
-    }
-
-    // 이벤트 해제
-    componentWillUnmount() {
-        // this.exitApp = false;
-        BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
-    }
-
-    render() {
-        return (
-            <WebView
-                source={{uri: this.state.webViewUrl}}
-                ref={webView => {
-                    this.webView = webView;
-                }}
-                javaScriptEnabled={true}
-                onLoad={this.onLoadWebView}
-                onMessage={this.invoke.listener}
-            />
-        );
-    }
+    openBrowser = url => {
+        if (Platform.OS === 'android') {
+            SendIntentAndroid.openChromeIntent(url)
+                .then(isOpened => {
+                    if (!isOpened) {
+                        alert('앱 실행이 실패했습니다');
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        } else {
+            // T0d0 ios 테스트 필요
+            Linking.openURL(url).catch(err => {
+                alert(
+                    '앱 실행이 실패했습니다. 설치가 되어있지 않은 경우 설치하기 버튼을 눌러주세요.',
+                );
+            });
+        }
+    };
 }
