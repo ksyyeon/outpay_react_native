47ba5dc1a523cf308507725f853e402e01758841
ksyyeon <suyeonmin@naver.com>
2021-10-18 17:30:37 +0900

    Firebase 연동

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {AppScreens} from './components/AppStack';
 import {SignInScreens} from './components/SignInStack';
 import Splash from './components/screens/Splash';
 import * as LocalStorage from './components/LocalStorage';
+import messaging from '@react-native-firebase/messaging';
+import {Alert} from 'react-native';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -14,6 +16,37 @@ export default class App extends React.Component {
             autoLogin: null,
             isLoading: true,
         };
+    }
+
+    // 이벤트 동작
+    async componentDidMount() {
+        // setTimeout(() => SplashScreen.hide(), 2000);
+        // TODO: ios에서 default 스플래시 비활성화
+        const result = await this.checkUserSignedIn();
+        setTimeout(() => {
+            this.requestUserPermssionForFcm();
+            this.setState({
+                isSignedIn: result.signedIn,
+                autoLogin: result.autoLogin,
+                isLoading: false,
+            });
+        }, 2000);
+    }
+
+    // 이벤트 해제
+    componentWillUnmount() {}
+
+    render() {
+        if (this.state.isLoading) return <Splash />;
+        return (
+            <NavigationContainer ref={RootNavigation.navigationRef}>
+                {this.state.isSignedIn ? (
+                    AppScreens(this.state.autoLogin)
+                ) : (
+                    <SignInScreens />
+                )}
+            </NavigationContainer>
+        );
     }
 
     checkUserSignedIn = async () => {
@@ -28,41 +61,48 @@ export default class App extends React.Component {
             // RootNavigation.navigate('MainWebView', {telNum: json['telNum']});
         } else {
             console.log('미등록회원');
-            return {signedIn: true, autoLogin: null};
+            return {signedIn: false, autoLogin: null};
             // RootNavigation.navigate('OnBoarding', null);
         }
     };
 
-    // 이벤트 동작
-    async componentDidMount() {
-        // setTimeout(() => SplashScreen.hide(), 2000);
-        // TODO: ios에서 default 스플래시 비활성화
-        const result = await this.checkUserSignedIn();
-        setTimeout(
-            () =>
-                this.setState({
-                    isSignedIn: result.signedIn,
-                    autoLogin: result.autoLogin,
-                    isLoading: false,
-                }),
-            2000,
-        );
-    }
+    requestUserPermssionForFcm = async () => {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+            authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+            messaging.AuthorizationStatus.PROVISIONAL;
 
-    // 이벤트 해제
-    componentWillUnmount() {}
+        if (enabled) {
+            const token = await messaging().getToken();
 
-    render() {
-        if (this.state.isLoading) return <Splash />;
+            console.log('fcm token:', token);
+            console.log('Authorization status:', authStatus);
 
-        return (
-            <NavigationContainer ref={RootNavigation.navigationRef}>
-                {this.state.isSignedIn ? (
-                    AppScreens(this.state.autoLogin)
-                ) : (
-                    <SignInScreens />
-                )}
-            </NavigationContainer>
-        );
-    }
+            this.handleFcmMessage();
+        } else {
+            console.log('fcm auth failed!');
+        }
+    };
+
+    handleFcmMessage = () => {
+        // 푸시를 받으면 호출
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            console.log(
+                'A new FCM message arrived!',
+                JSON.stringify(remoteMessage),
+            );
+            Alert.alert(
+                'A new FCM message arrived!',
+                JSON.stringify(remoteMessage),
+            );
+        });
+
+        // 알림창을 클릭한 경우 호출
+        messaging().onNotificationOpenedApp(remoteMessage => {
+            console.log(
+                'Notification caused app to open from background state:',
+                remoteMessage.notification,
+            );
+        });
+    };
 }
