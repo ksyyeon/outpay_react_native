e1663257b9874b7cba4fcf2ab82f96e53c2f7fe1
ksyyeon <suyeonmin@naver.com>
2022-05-25 10:44:14 +0900

    sync

--- components/Axios.js
+++ components/Axios.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
-const Axios = axios.create({
-    // baseURL: 'http://localhost:9090',
-    baseURL: 'http://192.168.0.34:3000',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-});
+export default (() => {
+    const Axios = axios.create({
+        baseURL: 'http://localhost:9090',
+        // baseURL: 'http://192.168.0.34:3000',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+    });
 
-export default Axios;
+    return Axios;
+})();
+
+// export default Axios;
