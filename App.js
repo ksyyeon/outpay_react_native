80919f23f848fe64e3f472e94097d75dfa40d7f0
ksyyeon <suyeonmin@naver.com>
2022-06-20 14:19:51 +0900

    App functional로

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import * as RootNavigation from './components/RootNavigation';
 import Splash from './components/screens/Splash';
@@ -13,73 +13,38 @@ import store from './store';
 import {connect, Provider} from 'react-redux';
 import * as actions from './actions';
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isSignedIn: false,
-            isLoading: true,
-            js: null,
-            networkUnsubscribe: null,
-            networkConnected: false,
-            userVars: null,
-        };
+export default App = () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    const [autoLogin, setAutoLogin] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [js, setJs] = useState('');
+    const [networkConnected, setNetworkConnected] = useState(null);
+    const [userVars, setUserVars] = useState(null);
 
-        console.log('dddddddddd', props);
-        mapStateToProps = state => ({
-            dialogProps: state.dialogProps,
-        });
-
-        CommonDialogContainer = connect(mapStateToProps, actions)(CommonDialog);
-    }
-
-    // 이벤트 동작
-    componentDidMount() {
+    useEffect(() => {
         // setTimeout(() => SplashScreen.hide(), 2000);
         // TODO: iOS에서 default 스플래시 비활성화
         setTimeout(async () => {
-            this.state.userVars = await localStorage.getUserVars();
-            this.checkNetworkConnected();
+            setUserVars(await localStorage.getUserVars());
+            checkNetworkConnected();
         }, 2000);
-    }
+        return () => {
+            // networkUnsubscribe();
+        };
+    }, []);
 
-    // 이벤트 해제
-    componentWillUnmount() {
-        // networkUnsubscribe();
-    }
-
-    render() {
-        if (this.state.isLoading) return <Splash />;
-        return (
-            <Provider store={store}>
-                <NavigationContainer ref={RootNavigation.navigationRef}>
-                    <CommonDialogContainer />
-                    {this.state.networkConnected ? (
-                        this.state.isSignedIn ? (
-                            AppScreens(this.state.autoLogin)
-                        ) : (
-                            SignInScreens()
-                        )
-                    ) : (
-                        <NetworkFail />
-                    )}
-                </NavigationContainer>
-            </Provider>
-        );
-    }
-
-    checkUserSignedIn = async () => {
+    const checkUserSignedIn = async () => {
         let cert = await AsyncStorage.getItem('OutpayCert');
         cert = JSON.parse(cert);
         console.log('[App] outpayCert:', cert);
-        console.log('[App] userVars: ', this.state.userVars);
-        if (cert !== null && this.state.userVars !== null) {
+        console.log('[App] userVars: ', userVars);
+        if (cert !== null && userVars !== null) {
             // TODO: 비밀번호 설정 안하고 종료했을 때도 미등록회원으로
             if (cert.userInfo.password.length === 6) {
                 console.log('[App] 등록회원');
                 return {
                     signedIn: true,
-                    autoLogin: this.state.userVars.autoLogin,
+                    autoLogin: userVars.autoLogin,
                 };
             }
         }
@@ -87,17 +52,43 @@ export default class App extends React.Component {
         return {signedIn: false, autoLogin: null};
     };
 
-    checkNetworkConnected = () => {
+    const checkNetworkConnected = () => {
         NetInfo.fetch().then(async state => {
-            const result = await this.checkUserSignedIn();
+            const result = await checkUserSignedIn();
             console.log('[App] Connection type', state.type);
             console.log('[App] Is connected?', state.isConnected);
-            this.setState({
-                isSignedIn: result.signedIn,
-                autoLogin: result.autoLogin,
-                isLoading: false,
-                networkConnected: state.isConnected,
-            });
+            setIsSignedIn(result.signedIn);
+            setAutoLogin(result.autoLogin);
+            setIsLoading(false);
+            setNetworkConnected(state.isConnected);
         });
     };
-}
+
+    const mapStateToProps = state => ({
+        dialogProps: state.dialogProps,
+    });
+
+    const CommonDialogContainer = connect(
+        mapStateToProps,
+        actions,
+    )(CommonDialog);
+
+    return isLoading ? (
+        <Splash />
+    ) : (
+        <Provider store={store}>
+            <NavigationContainer ref={RootNavigation.navigationRef}>
+                <CommonDialogContainer />
+                {networkConnected ? (
+                    isSignedIn ? (
+                        AppScreens(autoLogin)
+                    ) : (
+                        SignInScreens()
+                    )
+                ) : (
+                    <NetworkFail />
+                )}
+            </NavigationContainer>
+        </Provider>
+    );
+};
